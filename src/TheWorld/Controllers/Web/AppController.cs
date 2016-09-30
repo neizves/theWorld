ed2ab1b570bfe98ed20bc72639c41f50c769c6a4
Aleksandr Neizvestnyi <neizves@mail.ru>
2016-09-30 16:02:25 +0300

    Add MVC, Controller, Views, Layout, Validation, Models, jquery.validate

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheWorld.Controllers.Web
+{
+	public class AppController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+	}
+}
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -16,25 +16,44 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+        	services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
+//        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+//        {
+//            loggerFactory.AddConsole();
+//
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//            }
+//
+//            app.UseDefaultFiles();
+//            app.UseStaticFiles();
+//
 //            app.Run(async (context) =>
 //           {
 //              await context.Response.WriteAsync("Hello World!");
 //         });
-        }
+//        }
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			app.UseStaticFiles();
+
+			app.UseMvc(config =>
+					{
+					config.MapRoute(
+							name:"Default",
+							template:"{controller}/{action}/{id?}",
+							defaults: new {controller = "App", action = "Index" }
+							);
+					});
+		}
     }
 }
