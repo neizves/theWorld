f6eb35463312753e270f7a5ca52494f6d364b46b
Aleksandr Neizvestnyi <neizves@mail.ru>
2016-10-03 16:45:17 +0300

    Complete MVC6

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,16 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using TheWorld.ViewModels;
 using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld.Controllers.Web
 {
 	public class AppController : Controller
 	{
 		private IMailService _mailService;
+		private IConfigurationRoot _config;
 
-		public AppController(IMailService mailService)
+		public AppController(IMailService mailService, IConfigurationRoot config)
 		{
 			_mailService = mailService;
+			_config = config;
 		}
 
 		public IActionResult Index()
@@ -26,7 +29,18 @@ namespace TheWorld.Controllers.Web
 		[HttpPost]
 		public IActionResult Contact(ContactViewModel model)
 		{
-			_mailService.SendMail("shawn@aol.com", model.Email, "From TheWorld", model.Message);
+			if (model.Email.Contains("aol.com")) 
+			{
+				ModelState.AddModelError("Email", "We don't support AOL addresses");
+			}
+			if (ModelState.IsValid)
+			{
+				_mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+				ModelState.Clear();
+
+				ViewBag.UserMessage = "Message Sent";
+			}
 
 			return View();
 		}
